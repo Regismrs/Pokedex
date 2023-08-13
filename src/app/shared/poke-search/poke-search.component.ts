7d1f25cd9b76fa3ed349b57263e65934ea43005a
regismrs <regismrs@hotmail.com>
2023-08-12 22:22:52 -0300

    REFACTOR: Refeita a logica para alimentar o datalist

--- src/app/shared/poke-search/poke-search.component.ts
+++ src/app/shared/poke-search/poke-search.component.ts
@@ -5,22 +5,14 @@ import { AfterViewInit, Component, EventEmitter, Input, OnInit, Output } from '@
   templateUrl: './poke-search.component.html',
   styleUrls: ['./poke-search.component.scss']
 })
-export class PokeSearchComponent implements OnInit{
+export class PokeSearchComponent{
     @Output() public emmitSearch:EventEmitter<Array<{name: string, url:string}>> = new EventEmitter()
     public pokemonsListNames:Array<any> = []
     public pokemonsFiltered:Array<{name:string, url:string}> = []
     private lastKeywordSearched:string = ""
 
-    ngOnInit() {
-      if (this.pokemonsListNames.length && localStorage.getItem("PokemonsListNamesUrls") !== null) {
-        const jso = localStorage.getItem("PokemonsListNamesUrls") || ''
-        this.pokemonsListNames = JSON.parse(jso).sort(
-          (a:any, b:any) => (a.name > b.name) ? 1 : (a.name < b.name) ? -1 : 0)
-        console.info("TOTAL: " + this.pokemonsListNames.length)
-      }
-    }
-
     filterNames(keyword:string):void {
+      if (this.pokemonsListNames.length === 0) this.fetchListNames();
       this.pokemonsFiltered = this.pokemonsListNames.filter(p => p.name.indexOf(keyword.toLowerCase()) !== -1)
       console.log(this.pokemonsFiltered)
     }
@@ -29,6 +21,15 @@ export class PokeSearchComponent implements OnInit{
       if (keyword !== this.lastKeywordSearched) {
         this.lastKeywordSearched = keyword
         this.emmitSearch.emit(this.pokemonsFiltered)
+      }
+    }
+
+    fetchListNames() {
+      if (localStorage.getItem("PokemonsListNamesUrls") !== null) {
+        const jso = localStorage.getItem("PokemonsListNamesUrls") || ''
+        this.pokemonsListNames = JSON.parse(jso).sort(
+          (a:any, b:any) => (a.name > b.name) ? 1 : (a.name < b.name) ? -1 : 0)
+        console.info("TOTAL: " + this.pokemonsListNames.length)
       }
     }
 }
